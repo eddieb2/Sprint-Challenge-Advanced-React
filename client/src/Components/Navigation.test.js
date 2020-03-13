168d2818f8898f9ae89fcc7230842db74342b352
Edward Blanciak <blanciak.edward2@gmail.com>
2020-03-13 13:41:25 -0400

    added additional test

--- client/src/Components/Navigation.test.js
+++ client/src/Components/Navigation.test.js
@@ -1,9 +1,5 @@
 import React from "react";
-import {
-  render,
-  fireEvent,
-  getByDisplayValue
-} from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import Navigation from "./Navigation";
 
 // ANCHOR Start of Tests
@@ -17,4 +13,12 @@ test("button fires correctly and changes darkmode to true", () => {
   const darkModeButton = getByText(/dark mode/i);
   fireEvent.click(darkModeButton);
   expect(window.localStorage.darkMode).toBeTruthy();
+});
+
+test("body has a class of dark-mode", () => {
+  const { getByText } = render(<Navigation />);
+  const darkModeButton = getByText(/dark mode/i);
+  // console.log(darkModeButton);
+  expect(document.body.className).toBe("dark-mode");
+  fireEvent.click(darkModeButton);
 });
